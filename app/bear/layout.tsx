356704018029524108ff29d0491cf83e82b56c1e
Noe Tellez <noetlz10@gmail.com>
2026-03-12 19:14:01 -0500

    Update layout.tsx

--- app/bear/layout.tsx
+++ app/bear/layout.tsx
@@ -10,10 +10,10 @@ export default function RootLayout({
       <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-8">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://buymeacoffee.com/davidemarcoli"
+          href="Panda likes froststars"
           target="_blank"
           rel="noopener noreferrer"
-          data-umami-event="Support Developer"
+          data-umami-event=""
         >
           <Coffee />
           Support me →
